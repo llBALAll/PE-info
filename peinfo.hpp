0e353a70ba094208f8eea98351bab52067541b77
llBALAll <43710708+llBALAll@users.noreply.github.com>
2020-04-23 12:10:13 -0300

    Update peinfo.hpp

--- peinfo.hpp
+++ peinfo.hpp
@@ -6,19 +6,18 @@
 #define BUILD_DLL
 
 #ifdef BUILD_DLL
-#define DLL_EXPORT __declspec(dllexport)
+  #define DLL_EXPORT __declspec(dllexport)
 #else
-#define DLL_EXPORT __declspec(dllimport)
+  #define DLL_EXPORT __declspec(dllimport)
 #endif
 
 
 
 #ifdef __cplusplus
-extern "C" {
+  extern "C" {
 #endif
 
 
-
 #ifdef __cplusplus
-}
+  }
 #endif
