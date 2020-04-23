213a1cfb8741471e044f86330cd1f111a309ebba
llBALAll <43710708+llBALAll@users.noreply.github.com>
2020-04-23 13:52:11 -0300

    Update peinfo.cpp

--- peinfo.cpp
+++ peinfo.cpp
@@ -160,17 +160,13 @@ BOOL APIENTRY DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved) {
 		case DLL_PROCESS_ATTACH:
 			CreateThread(0, 0, &getPEinfo, 0, 0, 0);
 			// attach to process
-			// return FALSE to fail DLL load
 			break;
-
 		case DLL_PROCESS_DETACH:
 			// detach from process
 			break;
-
 		case DLL_THREAD_ATTACH:
 			// attach to thread
 			break;
-
         	case DLL_THREAD_DETACH:
 			// detach from thread
 			break;
